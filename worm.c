fc71860486070bd965706cc21150af47a593aca4
DRR9186 <rlord6499@gmail.com>
2025-03-23 13:33:59 +0300

    Add files via upload

--- worm.c
+++ worm.c
@@ -1,22 +1,71 @@
+#ifdef UNICODE
+#define UNICODE
+#endif
+
 #include <windows.h>
-#include <wchar.h>
 
-int wmain(int argc, wchar_t **argv)
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
-	PDWORD cChars = NULL;
-	HANDLE std = GetStdHandle(STD_OUTPUT_HANDLE);
+	const wchar_t CLASS_NAME[] = L"Sample Window Class";
 
-	if (std == INVALID_HANDLE_VALUE)
+	WNDCLASS wc = { };
+
+	wc.lpfnWndProc	 = WindowProc;
+	wc.hInstance	 = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+			0,
+			CLASS_NAME,
+			L"Learn to Program Windows",
+			WS_OVERLAPPEDWINDOW,
+
+			CW_USEDFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+
+			NULL,
+			NULL,
+			hInstance,
+			NULL
+			);
+	if (hwnd == NULL)
 	{
-		wprintf(L"HUY\n (%d)", GetLastError());
+		return 0;
 	}
 
-	if (argv[1])
-	{
-		WriteConsoleW(std, argv[1], wcslen(argv[1]), cChars, NULL);
-	}
+	ShowWindow(hwnd, nCmdShow);
 
-	CloseHandle(std);
+	MSG msg = { };
+	while (GetMessage(&msg, NULL, 0, 0) > 0)
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
 
 	return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+		case WM_DESTROY:
+			PostQuitMessage(0);
+			return 0;
+
+		case WM_PAINT:
+			{
+				PAINTSTRUCT ps;
+				HDC hdc = BeginPaint(hwnd, &ps);
+
+				FillRect(hdc, &ps.rcPaint, (HBRUSH) (COLOR_WINDOW+1));
+
+				EndPaint(hwnd, &pc);
+			}
+			return 0;
+	}
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
